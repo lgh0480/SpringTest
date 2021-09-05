ab886a0c8db5b21bd2fbe802f9312674df3a4e25
GeonHui <lgh0480@gmail.com>
2021-09-06 00:07:06 +0900

    ex02 employees list 작성

---
+++ ex02/src/main/java/com/yedam/app/member/service/MemberServiceImpl.java
@@ -0,0 +1,45 @@
+package com.yedam.app.member.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yedam.app.member.domain.MemberVO;
+import com.yedam.app.member.mapper.MemberMapper;
+@Service
+public class MemberServiceImpl implements MemberService {
+	
+	@Autowired MemberMapper memberMapper;
+	
+	@Override
+	public List<MemberVO> memberList() {
+		// TODO 전체조회
+		return memberMapper.memberList();
+	}
+
+	@Override
+	public MemberVO memberOneSelect(MemberVO vo) {
+		// TODO 회원 한건 조회
+		return memberMapper.memberOneSelect(vo);
+	}
+
+	@Override
+	public int memberInsert(MemberVO vo) {
+		// TODO 회원 등록
+		return memberMapper.memberInsert(vo);
+	}
+
+	@Override
+	public int memberDelete(MemberVO vo) {
+		// TODO 회원 삭제
+		return memberMapper.memberDelete(vo);
+	}
+
+	@Override
+	public int memberUpdate(MemberVO vo) {
+		// TODO 회원 수정
+		return memberMapper.memberUpdate(vo);
+	}
+
+}
